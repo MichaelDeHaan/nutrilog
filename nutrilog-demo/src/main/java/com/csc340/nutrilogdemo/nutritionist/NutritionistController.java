0bb1339009d656207d98e26f3401e4c181711af8
CavsUNCGaccount <cavillarrea@uncg.edu>
2024-03-24 15:04:59 -0400

    added a nutritionist controller

---
+++ nutrilog-demo/src/main/java/com/csc340/nutrilogdemo/nutritionist/NutritionistController.java
@@ -0,0 +1,16 @@
+package com.csc340.nutrilogdemo.nutritionist;
+
+import com.csc340.nutrilogdemo.food.Food;
+import com.csc340.nutrilogdemo.food.FoodService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+public class NutritionistController {
+
+    public NutritionistController(){
+
+    }
+
+}
